37ec621f24710a1c7f622ede4b674e3773912f53
tomas bombau <tomas.bombau@gmail.com>
2024-05-09 17:49:16 -0300

    VMTranslator modularizado, manejo de directorios y archivo terminado

--- projects/7/VMTranslator/VMTranslator.py
+++ projects/7/VMTranslator/VMTranslator.py
@@ -3,32 +3,45 @@ import os
 from Parser import Parser
 from CodeWriter import CodeWriter
 
+class VMTranslator():
+    def __init__(self, args):
+        self.args = args
+        self.type_of_file()
 
-def translate_file(input_file, output_dir):
-    file = Parser(input_file)
-    file_title = file.title()
-    file_commands = file.read_vm_file()
-    file_parsed = file.process_commands()
-    output_file = os.path.join(output_dir, file_title)
-    writer = CodeWriter(output_file)
-    writer.clear()
-    writer.write_file(file_parsed)
-    writer.close()
+    def type_of_file(self):
+        if os.path.isfile(self.args.file):
+            output_dir = os.path.dirname(self.args.file)
+            self.translate_file(self.args.file, output_dir)
+
+        elif os.path.isdir(self.args.file):
+            for file_name in os.listdir(self.args.file):
+                if file_name.endswith(".vm"):
+                    input_file = os.path.join(self.args.file, file_name)
+                    output_dir = self.args.file
+                    self.translate_file(input_file, output_dir)
+        else:
+            raise Exception("Neither a .vm file or a directory containing .vm files")
+        
+    def translate_file(self, input_file, output_dir):
+        #PARSE COMMANDS
+        file = Parser(input_file)
+        file_title = file.title()
+        file_commands = file.read_vm_file()
+        file_parsed = file.process_commands()
+        output_file = os.path.join(output_dir, file_title)
+        
+        #WRITE ASSEMBLY LANGUAGE
+        writer = CodeWriter(output_file)
+        writer.clear()
+        writer.write_file(file_parsed)
+        writer.close()
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Translate a Python file or directory.')
+    parser = argparse.ArgumentParser(description='Translate a Python file or directory to ASM language.')
     parser.add_argument('file', help='The file or directory to translate')
     args = parser.parse_args()
 
-    if os.path.isfile(args.file):
-        translate_file(args.file, os.path.dirname(args.file))
-
-    elif os.path.isdir(args.file):
-        for file_name in os.listdir(args.file):
-            if file_name.endswith(".vm"):
-                input_file = os.path.join(args.file, file_name)
-                translate_file(input_file, args.file)
-    else:
-        raise Exception("Neither a .vm file or a directory containing .vm files")
+    VMTranslator(args)
+    
 
