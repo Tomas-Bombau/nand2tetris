e7ba6efba99402d683fb3f45e9e9f2bb3fd16f9b
tomas bombau <tomas.bombau@gmail.com>
2024-05-25 14:56:48 -0300

    XML file for one file ok

--- projects/10/Syntax Analyzer/JackTokenizer.py
+++ projects/10/Syntax Analyzer/JackTokenizer.py
@@ -1,30 +1,33 @@
 import re
+import string
 
 class JackTokenizer:
     def __init__(self, file):
         self.filePath = file
         self.list = []
         self.keywords = ['class', 'constructor', 'function', 'method', 'field', 'static', 'var', 'int', 'char', 'boolean', 'void', 'true', 'false', 'null', 'this', 'let', 'do', 'if', 'else', 'while', 'return']
-        self.symbols = []
+        self.symbols = ['{','}','[',']','(',')','.',',',';','+','-','*','/','&','|','<','>','=','~']
         self.actualToken = 0
-        self.read()
+        self.writeXml()
 
     def read(self):
         with open(self.filePath, "r") as infile:
             for lines in infile:
                 lines = lines.strip()
                 lines = re.sub(r"(\/\/.*|\/\*[\s\S]*?\*\/)", '', lines)
-                lines = re.sub(r'([()\{\};])', r' \1 ', lines)
+                lines = re.sub(r'([()\{\};.])', r' \1 ', lines)
                 tokens = re.split(r'(\s+|[()\{\};])', lines)
                 tokens = [token for token in tokens if token.strip()]
                 self.list.extend(tokens)
-        print(self.list)
 
     def hasMoreTokens(self):
-        return self.actualToken > len(self.list) - 1
+        if self.actualToken > len(self.list) - 1:
+            return False
+        else:
+            return True
     
     def advance(self):
-        if self.HasMoreTokens() == True:
+        if self.hasMoreTokens() == True:
             self.actualToken += 1
         else:
             return
@@ -33,3 +36,48 @@ class JackTokenizer:
         actualToken = self.list[self.actualToken]
         if actualToken in self.keywords:
             return "KEYWORD"
+        elif actualToken in self.symbols:
+            return "SYMBOL"
+        elif actualToken.startswith(string.digits):
+            return "INT_CONST"
+        elif actualToken.startswith('"'):
+            return "STRING_CONST"
+        elif (actualToken.startswith(letter) for letter in string.ascii_letters):
+            return "IDENTIFIER"
+    
+    def keyword(self, token):
+        return f"<keyword> {token} </keyword>"
+    
+    def symbol(self, token):
+        return f"<symbol> {token} </symbol>"
+
+    def intVal(self, token):
+        return f"<intVal> {token} </intVal>"
+
+    def stringVal(self, token):
+        return f"<stringVal> {token} </stringVal>"
+    
+    def identifier(self, token):
+        return f"<identifier> {token} </identifier>"
+    
+    def writeLine(self, input):
+        with open(f"PRUEBA.xml", "a") as xmlFile:
+            xmlFile.write(f"{input}\n")
+
+    def writeXml(self):
+        self.read()
+        while self.hasMoreTokens():
+            token = self.list[self.actualToken]
+            tokenType = self.tokenType()
+            if tokenType == "KEYWORD":
+                self.writeLine(self.keyword(token))
+            elif tokenType == "SYMBOL":
+                self.writeLine(self.symbol(token))
+            elif tokenType == "INT_CONST":
+                self.writeLine(self.intVal(token))
+            elif tokenType == "STRING_CONST":
+                self.writeLine(self.stringVal(token))
+            elif tokenType == "IDENTIFIER":
+                self.writeLine(self.identifier(token))
+            self.advance()
+    
